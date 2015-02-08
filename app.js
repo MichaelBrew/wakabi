d6ee223c1b4e5523e4b299e6f557132d685cfefb
Michael Brew <michael.brew@movial.com>
2015-02-08 14:45:10 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var sys          = require('sys');
+
+var index    = require('./routes/index');
+var users    = require('./routes/users');
+var incoming = require('./routes/incoming')
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/incoming', incoming);
+app.use('/users', users);
+
+/*
+app.post('/incoming', function(req, res) {
+    var message = req.body.Body;
+    var from    = req.body.From;
+    sys.log('From: ' + from + ', Message: ' + message);
+
+    var twiml = '<?xml version="1.0" encoding="UTF-8" ?>n<Response>n<Sms>Thanks for your text, we\'ll be in touch.</Sms>n</Response>';
+    res.send(twiml, {'Content-Type':'text/xml'}, 200);
+});
+*/
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
---
+++ routes/incoming.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var sys     = require('sys');
+var twilio  = require('twilio');
+var router  = express.Router();
+
+var keywordRide = "RIDE";
+var availableLocations = [
+    "area1",
+    "area2",
+    "area3",
+    "area4",
+    "area5"
+];
+
+var rideStages = {
+    NOT_REQUESTED  : "haveNotRequested",
+    REQUESTED_RIDE : "requestedRide",
+    SENT_LOCATION  : "sentLocation",
+    SENT_TRAILER   : "sentTrailer"
+}
+
+function parseCookies (request) {
+    var list = {},
+        rc = request.headers.cookie;
+
+    rc && rc.split(';').forEach(function( cookie ) {
+        var parts = cookie.split('=');
+        list[parts.shift().trim()] = unescape(parts.join('='));
+    });
+
+    return list;
+}
+
+function requestLocation (sender, res) {
+    var locationXml = "";
+    for (var i = 1; i <= availableLocations.length; i++) {
+        locationXml += (i + ": " + availableLocations[i-1]);
+
+        if (i != availableLocations.length+1) {
+            locationXml += "\n";
+        }
+    }
+
+    var responseText = 'Please respond with the number corresponding to your location:\n' + locationXml;
+    var response = new twilio.TwimlResponse();
+    response.sms(responseText);
+    res.send(response.toString(), {
+        'Set-Cookie':'rideStage='+rideStages.REQUESTED_RIDE, 'Content-Type':'text/xml'
+    }, 200);
+}
+
+var reply = function(req, res, next) {
+    var message = req.body.Body;
+    var from    = req.body.From;
+    var cookies = parseCookies(req);
+    var rideStage = (cookies['rideStage'] == null ? rideStages.NOT_REQUESTED : cookies['rideStage']);
+
+    sys.log('From: ' + from + ', Message: ' + message + ', rideStage: ' + rideStage);
+
+    switch (rideStage) {
+        case rideStages.NOT_REQUESTED:
+            if (message.toUpperCase() === keywordRide) {
+                sys.log('Ride requested');
+
+                if (/* sender's number doesn't exist in riders DB*/0) {
+                        /* Add number to rider DB*/
+                }
+
+                /* Send response asking for location */
+                requestLocation(from, res);
+            }
+            break;
+
+        case rideStages.REQUESTED_RIDE:
+            sys.log('Asked for location');
+            break;
+
+        case rideStages.SENT_LOCATION:
+            sys.log('Received location');
+            break;
+
+        case rideStages.SENT_TRAILER:
+            sys.log('Received trailer decision');
+            break;
+    }
+}
+
+/* Incoming SMS */
+router.post('/', [reply]);
+
+module.exports = router;
