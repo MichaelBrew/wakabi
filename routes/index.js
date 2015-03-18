1b3c4e3e48b75d4ed885d2af97b293fd921b3f9c
Michael Brew <michael.brew@movial.com>
2015-03-17 19:38:06 -0700

    Trying to grab drivers for DB to display on homepage... i.e. idk what I'm doing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var sys = require('sys');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Wakabi' });
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+    if (!err) {
+        sys.log("index.js: Successfully connected to DB, about to query for drivers");
+        var query = client.query("SELECT num FROM drivers", function(err, result) {
+            if (!err) {
+              sys.log("index.js: Successfully got the drivers, there were " + result.rows.length);
+              res.render('index', { title: 'Wakabi', drivers: result.rows })
+            } else {
+              // Error
+              sys.log("index.js: Error querying DB for drivers, " + err);
+            }
+        });
+    } else {
+        // Error
+        sys.log("index.js: Error connecting to DB, " + err);
+    }
+  });
+
+  //res.render('index', { title: 'Wakabi' });
 });
 
 module.exports = router;
