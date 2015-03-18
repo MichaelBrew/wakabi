6ed82bab91018d9e317c6e05ba78eb3050ed5044
Michael Brew <michael.brew@movial.com>
2015-03-17 20:06:14 -0700

    ayy finally got it working

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,8 @@ var sys = require('sys');
 router.get('/', function(req, res, next) {
   pg.connect(process.env.DATABASE_URL, function(err, client) {
     if (!err) {
-        sys.log("index.js: Successfully connected to DB, about to query for drivers");
         var query = client.query("SELECT num FROM drivers", function(err, result) {
             if (!err) {
-              sys.log("index.js: Successfully got the drivers, there were " + result.rows.length);
-              sys.log("index.js: For example, result.rows[0] is " + result.rows[0]);
-              sys.log("index.js: result.rows[0].num is " + result.rows[0].num);
               res.render('index', { title: 'Wakabi', drivers: result.rows })
             } else {
               // Error
@@ -24,8 +20,6 @@ router.get('/', function(req, res, next) {
         sys.log("index.js: Error connecting to DB, " + err);
     }
   });
-
-  //res.render('index', { title: 'Wakabi' });
 });
 
 module.exports = router;
