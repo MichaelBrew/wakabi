9afa6b8995f517babdd97f3618feb0c014590ab1
Michael Brew <michael.brew@movial.com>
2015-05-10 18:02:42 -0700

    Cleaned up incoming.js

--- routes/incoming.js
+++ routes/incoming.js
@@ -5,120 +5,101 @@ var twilio       = require('twilio');
 var db           = require('../public/javascripts/db');
 var stages       = require('../public/javascripts/stages');
 
+var Messenger = require('../public/javascripts/TextMessenger');
 var RiderMessenger  = require('../public/javascripts/RiderMessenger');
 var DriverMessenger = require('../public/javascripts/DriverMessenger');
 
 var router       = express.Router();
 
-
 /******************/
 /* TEST FUNCTIONS */
 /******************/
 function isRideStageReset(res, msg) {
-    if (msg.toLowerCase() == "reset") {
-        sys.log("isRideStageReset: message was a reset");
-        var response = new twilio.TwimlResponse();
-        var responseText = "Ok, rideStage/driveStage has been reset to NOTHING";
-        response.sms(responseText);
-        res.cookie('rideStage', stages.rideStages.NOTHING);
-        res.cookie('driveStage', stages.driveStages.NOTHING);
-        sys.log("isRideStageReset: Just set the rideStage/driveStage to " + stages.rideStages.NOTHING);
-        res.send(response.toString(), {
-            'Content-Type':'text/xml'
-        }, 200);
+  if (msg.toLowerCase() == "reset") {
+    sys.log("isRideStageReset: message was a reset");
 
-        sys.log("isRideStageReset: returning true");
-        return true;
+    var message = "Ok, rideStage/driveStage has been reset to NOTHING";
+    var cookies = {
+      'rideStage': stages.rideStages.NOTHING,
+      'driveStage': stages.driveStages.NOTHING
     }
+    Messenger.textResponse(res, message, cookies);
 
-    return false;
+    sys.log("isRideStageReset: returning true");
+    return true;
+  }
+  return false;
 }
 
 function isQuickDriverSignUp(res, message, from) {
-    if (message.toLowerCase() == "signupdriver") {
-        pg.connect(process.env.DATABASE_URL, function(err, client) {
-            if (!err) {
-                sys.log("isQuickDriverSignUp: connected to DB");
-                // Create query to add driver
-                // TODO: Should probably get current date and use that, but not high priority
-                //       since this is just for internal testing anyway
-                var queryString = "INSERT INTO drivers (num, working, on_ride, current_zone, has_trailer, rating, last_payment) VALUES ('"
-                        + from + "', true, false, 1, true, 100, '2015-02-26')";
+  if (message.toLowerCase() == "signupdriver") {
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+      if (!err) {
+        sys.log("isQuickDriverSignUp: connected to DB");
+        // Create query to add driver
+        // TODO: Should probably get current date and use that, but not high priority
+        //       since this is just for internal testing anyway
+        var queryString = "INSERT INTO drivers (num, working, on_ride, current_zone, has_trailer, rating, last_payment) VALUES ('"
+          + from + "', true, false, 1, true, 100, '2015-02-26')";
 
-                var query = client.query(queryString, function(err, result) {
-                    var responseText = "";
-                    if (!err) {
-                        sys.log("isQuickDriverSignUp: Driver added to DB successfully");
-                        responseText += "Ok, you are now registered as a driver!";
-                        res.cookie('driveStage', stages.driveStages.NOTHING);
-                    } else {
-                        sys.log("isQuickDriverSignUp: Error adding driver to DB, " + err);
-                        responseText += "Error adding driver, " + err;
-                    }
-
-                    // Send response text to sender
-                    var response = new twilio.TwimlResponse();
-                    response.sms(responseText);
-                    res.send(response.toString(), {
-                        'Content-Type':'text/xml'
-                    }, 200);
-                });
-            } else {
-                // Error connecting to DB
-                var errorString = "Error connecting to DB to add driver, " + err;
-                sys.log("isQuickDriverSignUp: " + errorString);
-                var response = new twilio.TwimlResponse();
-                response.sms(errorString);
-                res.send(response.toString(), {
-                    'Content-Type':'text/xml'
-                }, 200);
+        var query = client.query(queryString, function(err, result) {
+          var responseText = "";
+          var cookies = {};
+          if (!err) {
+            sys.log("isQuickDriverSignUp: Driver added to DB successfully");
+            responseText += "Ok, you are now registered as a driver!";
+            cookies = {
+              'driveStage': stages.driveStages.NOTHING
             }
+          } else {
+            sys.log("isQuickDriverSignUp: Error adding driver to DB, " + err);
+            responseText += "Error adding driver, " + err;
+          }
+
+          Messenger.textResponse(res, responseText, cookies);
         });
-
-        return true;
-    }
-
-    return false;
+      } else {
+        // Error connecting to DB
+        var errorString = "Error connecting to DB to add driver, " + err;
+        Messenger.textResponse(res, errorString);
+        sys.log("isQuickDriverSignUp: " + errorString);
+      }
+    });
+    return true;
+  }
+  return false;
 }
 
 function isQuickRemoveDriver(res, message, from) {
-    if (message.toLowerCase() == "removedriver") {
-        pg.connect(process.env.DATABASE_URL, function(err, client) {
-            if (!err) {
-                var queryString = "DELETE FROM drivers WHERE num = '" + from + "'";
-                var query = client.query(queryString, function(err, result) {
-                    var responseText = "";
-                    if (!err) {
-                        sys.log("isQuickRemoveDriver: Driver removed from DB successfully");
-                        responseText += "Ok, you are no longer a driver!";
-                        res.cookie('rideStage', stages.rideStages.NOTHING);
-                    } else {
-                        sys.log("isQuickRemoveDriver: Error removing driver from DB, " + err);
-                        responseText += "Error removing driver, " + err;
-                    }
-
-                    // Send response to sender
-                    var response = new twilio.TwimlResponse();
-                    response.sms(responseText);
-                    res.send(response.toString(), {
-                        'Content-Type':'text/xml'
-                    }, 200);
-                });
-            } else {
-                var errorString = "Error connecting to DB to remove driver, " + err;
-                sys.log("isQuickRemoveDriver: " + errorString);
-                var response = new twilio.TwimlResponse();
-                response.sms(errorString);
-                res.send(response.toString(), {
-                    'Content-Type':'text/xml'
-                }, 200);
+  if (message.toLowerCase() == "removedriver") {
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+      if (!err) {
+        var queryString = "DELETE FROM drivers WHERE num = '" + from + "'";
+        var query = client.query(queryString, function(err, result) {
+          var responseText = "";
+          var cookies = {};
+          if (!err) {
+            sys.log("isQuickRemoveDriver: Driver removed from DB successfully");
+            responseText += "Ok, you are no longer a driver!";
+            cookies = {
+              'rideStage': stages.rideStages.NOTHING
             }
+          } else {
+            sys.log("isQuickRemoveDriver: Error removing driver from DB, " + err);
+            responseText += "Error removing driver, " + err;
+          }
+
+          Messenger.textResponse(res, responseText, cookies);
         });
-
-        return true;
-    }
-
-    return false;
+      } else {
+        var errorString = "Error connecting to DB to remove driver, " + err;
+        sys.log("isQuickRemoveDriver: " + errorString);
+        Messenger.textResponse(res, errorString);
+      }
+    });
+    return true;
+  }
+  return false;
 }
 
 
@@ -126,74 +107,74 @@ function isQuickRemoveDriver(res, message, from) {
 /* ROUTING FUNCTIONS */
 /*********************/
 function getStage(request, isDriver) {
-    var defaultReturnVal;
+  var defaultReturnVal;
 
+  if (isDriver) {
+    defaultReturnVal = stages.driveStages.NOTHING;
+  } else {
+    defaultReturnVal = stages.rideStages.NOTHING;
+  }
+
+  if (request.cookies != null) {
     if (isDriver) {
-        defaultReturnVal = stages.driveStages.NOTHING;
+      if (request.cookies.driveStage != null) {
+        return request.cookies.driveStage;
+      }
     } else {
-        defaultReturnVal = stages.rideStages.NOTHING;
+      if (request.cookies.rideStage != null) {
+        return request.cookies.rideStage;
+      }
     }
+  }
 
-    if (request.cookies != null) {
-        if (isDriver) {
-            if (request.cookies.driveStage != null) {
-                return request.cookies.driveStage;
-            }
-        } else {
-            if (request.cookies.rideStage != null) {
-                return request.cookies.rideStage;
-            }
-        }
-    }
-
-    sys.log("getStage: cookies are null, or cookies.stage was null, returning '" + defaultReturnVal + "'");
-    return defaultReturnVal;
+  sys.log("getStage: cookies are null, or cookies.stage was null, returning '" + defaultReturnVal + "'");
+  return defaultReturnVal;
 }
 
 var receiveIncomingMessage = function(req, res, next) {
 
-    var message   = req.body.Body;
-    var from      = req.body.From;
+  var message   = req.body.Body;
+  var from      = req.body.From;
 
-    sys.log('From: ' + from + ', Message: ' + message);
+  sys.log('From: ' + from + ', Message: ' + message);
 
-    // Testing shortcuts
-    if (isRideStageReset(res, message)) {
-        return;
-    } else if (isQuickDriverSignUp(res, message, from)) {
-        return;
-    } else if (isQuickRemoveDriver(res, message, from)) {
-        return;
-    }
+  // Testing shortcuts
+  if (isRideStageReset(res, message)) {
+    return;
+  } else if (isQuickDriverSignUp(res, message, from)) {
+    return;
+  } else if (isQuickRemoveDriver(res, message, from)) {
+    return;
+  }
 
-    pg.connect(process.env.DATABASE_URL, function(err, client) {
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+    if (!err) {
+      // Check if sender is a driver
+      var query = client.query("SELECT num FROM drivers WHERE num = '" + from + "'", function(err, result) {
         if (!err) {
-            // Check if sender is a driver
-            var query = client.query("SELECT num FROM drivers WHERE num = '" + from + "'", function(err, result) {
-                if (!err) {
-                    if (result.rows.length == 0) {
-                        sys.log("receiveIncomingMessage: sender is a rider");
-                        RiderMessenger.handleText(req, res, message, from, getStage(req, false));
-                    } else {
-                        sys.log("receiveIncomingMessage: sender is a driver");
-                        DriverMessenger.handleText(res, message, from, getStage(req, true));
-                    }
-                } else {
-                    sys.log("receiveIncomingMessage: Error querying DB to see if driver exists already, " + err);
-                    // Default to rider
-                    RiderMessenger.handleText(req, res, message, from, getStage(req, false));
-                }
-
-                client.end();
-                sys.log("incoming.js: closed connection to DB");
-            });
-
-        } else {
-            sys.log("receiveIncomingMessage: Error connecting to DB, " + err);
-            // Default to rider
+          if (result.rows.length == 0) {
+            sys.log("receiveIncomingMessage: sender is a rider");
             RiderMessenger.handleText(req, res, message, from, getStage(req, false));
+          } else {
+            sys.log("receiveIncomingMessage: sender is a driver");
+            DriverMessenger.handleText(res, message, from, getStage(req, true));
+          }
+        } else {
+          sys.log("receiveIncomingMessage: Error querying DB to see if driver exists already, " + err);
+          // Default to rider
+          RiderMessenger.handleText(req, res, message, from, getStage(req, false));
         }
-    });
+
+        client.end();
+        sys.log("incoming.js: closed connection to DB");
+      });
+
+    } else {
+      sys.log("receiveIncomingMessage: Error connecting to DB, " + err);
+      // Default to rider
+      RiderMessenger.handleText(req, res, message, from, getStage(req, false));
+    }
+  });
 }
 
 /* Incoming SMS */
