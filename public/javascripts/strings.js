9798f23ed7e1e505f2062bac333aa249e5331062
Michael Brew <michael.brew@movial.com>
2015-05-12 15:39:47 -0700

    Changed the area names to actual cities/villages in Uganda

--- public/javascripts/strings.js
+++ public/javascripts/strings.js
@@ -1,28 +1,28 @@
 exports.keywordRide = "RIDE";
 exports.availableLocations = [
-    "area1",
-    "area2",
-    "area3",
-    "area4",
-    "area5"
+  "Kawempe",
+  "Nangaiza",
+  "Mubende",
+  "Iganga",
+  "Arua"
 ];
 exports.resendText = "We\'re sorry, we did not understand that message. ";
 exports.askLocation = "Please respond with the number corresponding to your location:\n";
 exports.askTrailer = "Do you need a trailer to transport cargo (answer yes or no)? It will cost extra.";
 exports.helpText = "Please text RIDE to request a ride.";
 exports.validYesWords = [
-        "Y",
-        "y",
-        "YES",
-        "Yes",
-        "yes"
+  "Y",
+  "y",
+  "YES",
+  "Yes",
+  "yes"
 ];
 exports.validNoWords = [
-        "N",
-        "n",
-        "NO",
-        "No",
-        "no"
+  "N",
+  "n",
+  "NO",
+  "No",
+  "no"
 ];
 exports.waitText = "Ok, a driver will call you to pick you up shortly!";
 exports.noDriversAvailable = "We're sorry, no drivers are available."
