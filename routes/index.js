a7ff334bc7a6e21279e37fc069a93c48365eb669
Michael Brew <michael.brew@movial.com>
2015-05-12 20:04:30 -0700

    Try to show driver stats on admin dashboard

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,23 @@ var moment = require('moment');
 router.get('/', function(req, res, next) {
   pg.connect(process.env.DATABASE_URL, function(err, client) {
     if (!err) {
-      var query = client.query("SELECT num FROM drivers", function(err, result) {
+      var query = client.query("SELECT * FROM drivers", function(err, result) {
         if (!err) {
+          var numDrivers = result.rows.length;
+          var numIdleDrivers = 0;
+          var numBusyDrivers = 0;
+
+          for driver in result.rows
+            if driver.giving_ride_to == null
+              numIdleDrivers++;
+            else
+              numBusyDrivers++;
+
           res.render('index', { 
             title: 'Wakabi', 
-            drivers: result.rows,
+            numDrivers: numDrivers
+            numIdleDrivers: numIdleDrivers
+            numBusyDrivers: numBusyDrivers
             date: moment().format('MMMM DD, YYYY')
           })
         } else {
