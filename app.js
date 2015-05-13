dfa8cd2f7f9a92f31f8e5d1d43c6b9f8788ab1df
Michael Brew <michael.brew@movial.com>
2015-05-12 19:14:28 -0700

    Display the current date on the admin dashboard

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express      = require('express');
 var path         = require('path');
 var favicon      = require('serve-favicon');
 var logger       = require('morgan');
+var moment       = require('moment');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var sys          = require('sys');
--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,17 @@ var sys = require('sys');
 router.get('/', function(req, res, next) {
   pg.connect(process.env.DATABASE_URL, function(err, client) {
     if (!err) {
-        var query = client.query("SELECT num FROM drivers", function(err, result) {
-            if (!err) {
-              res.render('index', { title: 'Wakabi', drivers: result.rows })
-            } else {
-              // Error
-              sys.log("index.js: Error querying DB for drivers, " + err);
-              res.render('index', { title: 'Wakabi', drivers: null })
-            }
-        });
+      var query = client.query("SELECT num FROM drivers", function(err, result) {
+        if (!err) {
+          res.render('index', { title: 'Wakabi', drivers: result.rows })
+        } else {
+          sys.log("index.js: Error querying DB for drivers, " + err);
+          res.render('index', { title: 'Wakabi', drivers: null })
+        }
+      });
     } else {
-        // Error
-        sys.log("index.js: Error connecting to DB, " + err);
-        res.render('index', { title: 'Wakabi', drivers: null })
+      sys.log("index.js: Error connecting to DB, " + err);
+      res.render('index', { title: 'Wakabi', drivers: null })
     }
   });
 });
