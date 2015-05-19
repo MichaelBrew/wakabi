e7b60f020fd61e52272bec02a51c03f65b0785a2
Michael Brew <michael.brew@movial.com>
2015-05-19 15:56:10 -0700

    On web dashboard, show rider and driver stats

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,20 @@ var moment = require('moment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var params = {
+    title: 'Wakabi',
+    date: moment().format('MMMM DD, YYYY'),
+    numDrivers: 0,
+    numIdleDrivers: 0,
+    numBusyDrivers: 0,
+    ridesRequested: 0,
+    ridesCompleted: 0,
+    ridesFailed: 0
+  }
+
   pg.connect(process.env.DATABASE_URL, function(err, client) {
     if (!err) {
-      var query = client.query("SELECT * FROM drivers", function(err, result) {
+      var query = client.query("SELECT * FROM drivers WHERE working = true", function(err, result) {
         if (!err) {
           var numDrivers = result.rows.length;
           var numIdleDrivers = 0;
@@ -22,21 +33,48 @@ router.get('/', function(req, res, next) {
             }
           }
 
-          res.render('index', { 
-            title: 'Wakabi', 
-            numDrivers: numDrivers,
-            numIdleDrivers: numIdleDrivers,
-            numBusyDrivers: numBusyDrivers,
-            date: moment().format('MMMM DD, YYYY')
-          })
+          params.numDrivers = numDrivers;
+          params.numIdleDrivers = numIdleDrivers;
+          params.numBusyDrivers = numBusyDrivers;
+
+          currentDay = moment().startOf('day').toDate()
+
+          var query = client.query("SELECT * FROM rides WHERE request_time >= " + currentDay, function(err, result) {
+            if (!err) {
+              if (result.rows.length == 0) {
+                sys.log("No rides have been requested today")
+
+                ridesRequested = result.rows.length
+                ridesCompleted = 0
+
+                for (var ride in result.rows) {
+                  if (ride.end_time != null) {
+                    ridesCompleted++
+                  }
+                }
+
+                ridesFailed =  ridesRequested - ridesCompleted
+
+                params.ridesRequested = ridesRequested
+                params.ridesCompleted = ridesCompleted
+                params.ridesRequested = ridesFailed
+              } else {
+                sys.log("At least 1 ride has been requested today!")
+              }
+            } else {
+              sys.log("index.js: Error connecting to DB, " + err)
+            }
+
+            res.render('index', params)
+          });
         } else {
-          sys.log("index.js: Error querying DB for drivers, " + err);
-          res.render('index', { title: 'Wakabi', drivers: null })
+          sys.log("index.js: Error querying DB for drivers, " + err)
+          res.render('index', params)
         }
       });
     } else {
-      sys.log("index.js: Error connecting to DB, " + err);
-      res.render('index', { title: 'Wakabi', drivers: null })
+      sys.log("index.js: Error connecting to DB, " + err)
+      res.render('index', params)
     }
   });
 });
