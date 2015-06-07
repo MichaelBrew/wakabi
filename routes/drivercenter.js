7cb0dd903469b83d9077ee3211e237dd64a8636b
Michael Brew <mike_brew1234@yahoo.com>
2015-06-06 18:54:58 -0700

    Fixing up driver center

--- routes/drivercenter.js
+++ routes/drivercenter.js
@@ -14,6 +14,13 @@ router.get('/', function(req, res, next) {
       var query = client.query("SELECT * FROM drivers", function(err, result) {
         if (!err) {
           params.drivers = result.rows
+
+          for (var key in params.drivers) {
+            if (params.drivers.hasOwnProperty(key)) {
+              params.drivers[k] = params.drivers[k].replace(/[^\d\+]/g, ''); // STRIP out < >
+            }
+          }
+
           params.removeDriver = function(driverNum) {
             sys.log("wanna remove " + driverNum);
           }
