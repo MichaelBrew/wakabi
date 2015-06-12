39a54992d32ba962372fbe06a18e79242eb0be59
Michael Brew <mike_brew1234@yahoo.com>
2015-06-11 20:29:03 -0700

    Think I was selecting the wrong thing with jquery

--- public/javascripts/drivercenter.js
+++ public/javascripts/drivercenter.js
@@ -4,7 +4,7 @@ function removeDriverClicked(driverNum) {
   console.log("removeDriverClicked: driverNum = " + driverNum)
 
   removeDriverNum = driverNum
-  $(".modal-body").val("Are you sure you want to remove driver " + removeDriverNum + "?")
+  $("#modal-text").val("Are you sure you want to remove driver " + removeDriverNum + "?")
 }
 
 function removeDriver() {
