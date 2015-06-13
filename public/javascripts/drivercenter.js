ac7297408e724052a49e794735c3af06df4d5ae1
Michael Brew <mike_brew1234@yahoo.com>
2015-06-12 17:54:28 -0700

    Show spinner after clicking yes to remove driver

--- public/javascripts/drivercenter.js
+++ public/javascripts/drivercenter.js
@@ -4,11 +4,12 @@ function removeDriverClicked(driverNum) {
   console.log("removeDriverClicked: driverNum = " + driverNum)
 
   removeDriverNum = driverNum
-  $(".modal-text").text("Are you sure you want to remove driver " + removeDriverNum + "?")
+  $(".modal-text").text("Are you sure you want to remove driver +" + removeDriverNum + "?")
 }
 
 function removeDriver() {
   console.log("Attempting to remove driver " + removeDriverNum)
+  $(".modal-body").html("<div class='spinner-loader'>Loading…</div>")
 
   //onClick="removeDriver('#{driver.num}')
 }
