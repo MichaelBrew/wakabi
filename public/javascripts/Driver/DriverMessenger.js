277ba622159d77b76d81eeb884dd41cb6d6678bc
Michael Brew <michael@onerent.co>
2017-05-07 01:56:48 -0700

    Refactor DriverMessenger/DriverUtil

--- public/javascripts/Driver/DriverMessenger.js
+++ public/javascripts/Driver/DriverMessenger.js
@@ -1,217 +1,163 @@
-var sys     = require('sys')
-var pg      = require('pg')
-var _ = require('underscore')
-var moment = require('moment')
+const moment = require('moment')
 
-var stages  = require('../stages')
-var strings = require('../strings')
-var parser  = require('../messageParser')
-var db      = require('../db')
-var DriverUtil = require('./DriverUtil')
-var Messenger = require('../TextMessenger')
-var RiderWaitingQueue = require('../Rider/RiderWaitingQueue')
-var RiderMessenger = require('../Rider/RiderMessenger')
+const STAGES = require('../stages')
+const STRINGS = require('../strings')
 
-function requestLocation(res, resend, stage) {
-  cookies = {"driveStage": stage}
-  Messenger.requestLocation(res, resend, cookies)
+const parser = require('../messageParser')
+const db = require('../db')
+const DriverUtil = require('./DriverUtil')
+const PgUtil = require('../../../util/pg')
+const Messenger = require('../TextMessenger')
+
+function requestLocation(res, resend, driveStage) {
+  return Messenger.requestLocation(res, resend, {driveStage})
 }
 
-function receiveStartShiftLocation(res, location, from) {
-  DriverUtil.toggleDriverShift(res, from, true, function(err) {
-    if (!err) {
-      DriverUtil.updateLocation(from, +location, function(err) {
-        var responseText = ""
-
-        if (!err) {
-          responseText = strings.successfulStartShift
-          checkRiderWaitingQueue(from, location)
-        } else {
-          responseText = strings.dbError
-        }
-
-        cookies = {"driveStage": stages.driveStages.NOTHING}
-        Messenger.textResponse(res, responseText, cookies)
-      })
-    } else {
-      cookies = {"driveStage": stages.driveStages.NOTHING}
-      Messenger.textResponse(res, strings.dbError, cookies)
-    }
-  })
-}
-
-function checkRiderWaitingQueue(driverNum, location) {
-  ridersWaiting = RiderWaitingQueue.getRidersWaitingInZone(location)
-
-  if (ridersWaiting.length > 0) {
-    Messenger.text(driverNum, strings.acceptRideQuestion)
-  }
-}
-
-function handleRequestResponse(res, message, from) {
-  if (parser.isYesMessage(message)) {
-    sendNumberToDriver(res, from)
-  } else if (parser.isNoMessage(message)) {
-    // DriverUtil.getDriverWithNum(from, function(err, driver) {
-    //   if (!err) {
-        
-    //   }
-    // })
-
-    db.getDriverFromNum(from, function(driver) {
-      if (driver) {
-        pg.connect(process.env.DATABASE_URL, function(err, client) {
-          if (!err) {
-            var queryString = "SELECT * FROM rides WHERE driver_num = '" + from + "' AND end_time IS NULL"
-            var query = client.query(queryString, function(err, result) {
-              if (!err) {
-                var ride = result.rows[0]
-                var driverNum = ride.driver_num
-
-                var queryString = "UPDATE rides SET driver_num = NULL WHERE ride_id = " + ride.ride_id
-                var query = client.query(queryString, function(err, result) {
-                  if (!err) {
-                    var params = {
-                      rideId: ride.ride_id,
-                      driverTimeLastRide: driver.time_last_ride,
-                      riderWaitingForResponse: true
-                    }
-
-                    db.sendRequestToAvailableDriver(params)
-                  }
-                })
-              }
-            })
-          }
-        })
-      }
-    })
-  } else {
-    // wasn't a response to the request, send back default message?
-  }
+function receiveStartShiftLocation(res, location, driverNum) {
+  return DriverUtil.toggleDriverShift(driverNum, true)
+    .then(() => DriverUtil.updateLocation(driverNum, +location))
+    .then(() => Messenger.textResponse(res, STRINGS.successfulStartShift, {
+      driveStage: STAGES.driveStages.NOTHING
+    }))
+    .catch(() => Messenger.textResponse(res, STRINGS.dbError, {
+      driveStage: STAGES.driveStages.NOTHING
+    }))
 }
 
 function sendNumberToDriver(res, driverNum) {
-  pg.connect(process.env.DATABASE_URL, function(err, client) {
-    if (!err) {
-      var queryString = "UPDATE rides SET status = 'ACTIVE' WHERE driver_num = '" + driverNum + "' AND end_time IS NULL RETURNING rider_num"
-      var query = client.query(queryString, function(err, result) {
-        if (!err) {
-          var riderNum = result.rows[0].rider_num
-          var responseText = strings.hereIsRiderNum + riderNum
+  const queryString = `
+    UPDATE
+      rides
+    SET
+      status = 'ACTIVE'
+    WHERE
+      driver_num = '${driverNum}' AND end_time IS NULL RETURNING rider_num
+  `
 
-          cookies = {"driveStage": stages.driveStages.AWAITING_END_RIDE}
-          Messenger.textResponse(res, responseText, cookies)
-
-          RiderWaitingQueue.removeRiderFromQueue(riderNum)
-        }
-        client.end();
-      });
-    }
-  });
+  return PgUtil.query(queryString).then(({rows: riders}) => {
+    return Messenger.textResponse(res, `${STRINGS.hereIsRiderNum}${riders[0].rider_num}`, {
+      driveStage: STAGES.driveStages.AWAITING_END_RIDE
+    })
+  })
 }
 
-function handleEndRideText(res, message, from) {
-  if (parser.isEndRideMessage(message)) {
-    pg.connect(process.env.DATABASE_URL, function(err, client) {
-      if (!err) {
-        var queryString = "SELECT * FROM rides WHERE driver_num = '" + from + "' AND end_time IS NULL"
-        var query = client.query(queryString, function(err, result) {
-          if (!err) {
-            var ride = result.rows[0]
-            var riderNum = ride.rider_num
-            var endTime = moment().format('YYYY-MM-DD HH:mm:ssZ')
-
-            Messenger.text(riderNum, strings.feedbackQuestion)
-            requestLocation(res, false, stages.driveStages.AWAITING_UPDATED_LOCATION)
-
-            var queryString = "UPDATE rides SET end_time = '" + endTime + "', status = 'FINISHED' WHERE ride_id = " + ride.ride_id
-            var query = client.query(queryString, function(err, result) {
-              if (!err) {
-                var queryString = "UPDATE drivers SET time_last_ride = '" + endTime + "' WHERE num = '" + from + "'"
-                var query = client.query(queryString, function(err, result) {
-                  if (!err) {
-                    // Timestamp set
-                  }
-                  client.end()
-                })
-              }
-            })
-          }
-        })
+function handleRequestResponse(res, message, driverNum) {
+  if (parser.isYesMessage(message)) {
+    return sendNumberToDriver(res, driverNum)
+  } else if (parser.isNoMessage(message)) {
+    return DriverUtil.getDriverWithNum(driverNum).then(driver => {
+      if (!driver) {
+        return null
       }
+
+      const getRidesQuery =
+        `SELECT * FROM rides WHERE driver_num = '${driverNum}' AND end_time IS NULL`
+
+      return PgUtil.query(getRidesQuery).then(({rows: rides}) => {
+        const [ride] = rides
+
+        return PgUtil.query(`UPDATE rides SET driver_num = NULL WHERE ride_id = ${ride.ride_id}`)
+          .then(() => {
+            return db.sendRequestToAvailableDriver({
+              rideId: ride.ride_id,
+              driverTimeLastRide: driver.time_last_ride,
+              riderWaitingForResponse: true
+            })
+          })
+      })
     })
   }
 }
 
-function handleUpdatedLocation(res, message, driverNum) {
-  pg.connect(process.env.DATABASE_URL, function(err, client) {
-    if (!err) {
-      var queryString = "UPDATE drivers SET current_zone = " + (+message) + " WHERE num = '" + driverNum + "'";
-      var query = client.query(queryString, function(err, result) {
-        if (!err) {
-          cookies = {"driveStage": stages.driveStages.NOTHING}
-          Messenger.textResponse(res, strings.updatedDriverLocation, cookies)
-          checkRiderWaitingQueue(driverNum, +message)
+function handleEndRideText(res, message, driverNum) {
+  if (!parser.isEndRideMessage(message)) {
+    return Promise.resolve()
+  }
 
-          var queryString = "UPDATE rides SET destination = " + (+message) + " WHERE driver_num = '" + driverNum + "' AND destination IS NULL"
-          var query = client.query(queryString, function(err, result) {
-            client.end()
-          })
-        } else {
-          sys.log("handleUpdatedLocation: Error querying db, err: " + err);
-        }
-      })
-    }
+  const getRidesQuery =
+    `SELECT * FROM rides WHERE driver_num = '${driverNum}' AND end_time IS NULL`
+
+  return PgUtil.query(getRidesQuery).then(({rows: rides}) => {
+    const rideId = rides[0].ride_id
+    const riderNum = rides[0].rider_num
+    const rideEndTime = moment().format('YYYY-MM-DD HH:mm:ssZ')
+
+    const updateRideQuery =
+      `UPDATE rides SET end_time = '${rideEndTime}', status = 'FINISHED' WHERE ride_id = ${rideId}`
+    const updateDriverQuery =
+      `UPDATE drivers SET time_last_ride = '${rideEndTime}' WHERE num = '${driverNum}'`
+
+    return Promise.all([
+      requestLocation(res, false, STAGES.driveStages.AWAITING_UPDATED_LOCATION),
+      Messenger.text(riderNum, STRINGS.feedbackQuestion),
+      PgUtil.query(updateRideQuery),
+      PgUtil.query(updateDriverQuery)
+    ])
   })
 }
 
-function isShiftChange(res, message, from, driveStage) {
-  if (driveStage !== stages.driveStages.AWAITING_END_RIDE) {
-    if (parser.isStartShift(message)) {
-      requestLocation(res, false, stages.driveStages.AWAITING_START_LOCATION)
-      return true
-    } else if (parser.isEndShift(message)) {
-      DriverUtil.toggleDriverShift(from, false, function(err) {
-        if (!err) {
-          cookies = {"driveStage": stages.driveStages.NOTHING}
-          Messenger.textResponse(res, strings.successfulEndShift, cookies)
-        } else {
-          Messenger.textResponse(res, strings.dbError)
-        }
-      })
-      return true
-    }
-  }
+function handleUpdatedLocation(res, message, driverNum) {
+  return PgUtil.query(`UPDATE drivers SET current_zone = ${+message} WHERE num = '${driverNum}'`)
+    .then(() => {
+      const updateRideQuery = `
+        UPDATE
+          rides
+        SET
+          destination = ${+message}
+        WHERE
+          driver_num = '${driverNum}' AND destination IS NULL
+      `
 
-  return false
+      return Promise.all([
+        PgUtil.query(updateRideQuery),
+        Messenger.textResponse(res, STRINGS.updatedDriverLocation, {
+          driveStage: STAGES.driveStages.NOTHING
+        })
+      ])
+    })
+}
+
+function isShiftChange(message) {
+  return (parser.isStartShift(message)) || (parser.isEndShift(message))
+}
+
+function processShiftChange(res, message, driverNum) {
+  if (parser.isStartShift(message)) {
+    return requestLocation(res, false, STAGES.driveStages.AWAITING_START_LOCATION)
+  } else if (parser.isEndShift(message)) {
+    return DriverUtil.toggleDriverShift(driverNum, false)
+      .then(() => Messenger.textResponse(res, STRINGS.successfulEndShift, {
+        driveStage: STAGES.driveStages.NOTHING
+      }))
+      .catch(() => Messenger.textResponse(res, STRINGS.dbError))
+  }
 }
 
 module.exports = {
-  handleText: function(res, message, from, driveStage) {
-    if (isShiftChange(res, message, from, driveStage)) {
-      return
+  handleText: (res, message, driverNum, driverStage) => {
+    if (isShiftChange(message)) {
+      return processShiftChange(res, message, driverNum)
     }
-    
-    switch (driveStage) {
-      case stages.driveStages.AWAITING_START_LOCATION:
-        receiveStartShiftLocation(res, message, from);
-        break;
 
-      case stages.driveStages.NOTHING:
-        handleRequestResponse(res, message, from);
-        break;
+    switch (driverStage) {
+      case STAGES.driveStages.AWAITING_START_LOCATION:
+        return receiveStartShiftLocation(res, message, driverNum)
 
-      case stages.driveStages.AWAITING_END_RIDE:
-        handleEndRideText(res, message, from);
-        break;
+      case STAGES.driveStages.NOTHING:
+        return handleRequestResponse(res, message, driverNum)
 
-      case stages.driveStages.AWAITING_UPDATED_LOCATION:
-        handleUpdatedLocation(res, message, from);
-        break;
+      case STAGES.driveStages.AWAITING_END_RIDE:
+        return handleEndRideText(res, message, driverNum)
+
+      case STAGES.driveStages.AWAITING_UPDATED_LOCATION:
+        return handleUpdatedLocation(res, message, driverNum)
+
+      default:
+        console.error(`Unrecognized driver stage ${driverStage}`)
     }
   },
-  textDriverForConfirmation: function(driverNumber, riderNumber) {
-    Messenger.text(driverNumber, strings.acceptRideQuestion)
+
+  textDriverForConfirmation: (driverNumber) => {
+    return Messenger.text(driverNumber, STRINGS.acceptRideQuestion)
   }
-};
+}
--- public/javascripts/Driver/DriverUtil.js
+++ public/javascripts/Driver/DriverUtil.js
@@ -1,59 +1,22 @@
-var sys = require('sys')
-var pg = require('pg')
+const PgUtil = require('../../../util/pg')
 
-module.exports = {
-  toggleDriverShift: function(from, starting, cb) {
-    pg.connect(process.env.DATABASE_URL, function(err, client) {
-      if (!err) {
-        var queryString = "UPDATE drivers SET working = " + starting + " WHERE num = '" + from + "'"
-        var query = client.query(queryString, function(err, result) {
-          client.end()
-
-          if (err) {
-            cb(err)
-          } else {
-            cb()
-          }
-        })
-      } else {
-        cb(err)
+function getDriverWithNum(driverNum) {
+  return PgUtil.query(`SELECT * FROM drivers WHERE num = '${driverNum}'`)
+    .then(({rows: drivers}) => {
+      if (drivers.length === 0) {
+        return Promise.reject(`No driver found with number ${driverNum}`)
       }
-    })
-  },
-  updateLocation: function(driverNum, location, cb) {
-    pg.connect(process.env.DATABASE_URL, function(err, client) {
-      if (!err) {
-        var queryString = "UPDATE drivers SET current_zone = " + location + " WHERE num = '" + driverNum + "'"
-        var query = client.query(queryString, function(err, result) {
-          client.end()
 
-          if (err) {
-            cb(err)
-          } else {
-            cb()
-          }
-        })
-      } else {
-        cb(err)
-      }
+      return drivers[0]
     })
-  },
-  getDriverWithNum: function(driverNum, cb) {
-    pg.connect(process.env.DATABASE_URL, function(err, client) {
-      if (!err) {
-        var queryString = "SELECT * FROM drivers WHERE num = '" + driverNum + "'"
-        var query = client.query(queryString, function(err, result) {
-          client.end()
-
-          if (err) {
-            cb(err)
-          } else if (result.rows.length == 0) {
-            cb("No driver found with number")
-          } else {
-            cb(null, result.rows[0])
-          }
-        })
-      }
-    })
-  }
 }
+
+function toggleDriverShift(driverNum, isWorking) {
+  return PgUtil.query(`UPDATE drivers SET working = ${isWorking} WHERE num = '${driverNum}'`)
+}
+
+function updateLocation(driverNum, location) {
+  return PgUtil.query(`UPDATE drivers SET current_zone = ${location} WHERE num = '${driverNum}'`)
+}
+
+module.exports = {getDriverWithNum, toggleDriverShift, updateLocation}
